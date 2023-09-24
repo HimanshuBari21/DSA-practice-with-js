6538b5baea1bccd7d6371121d4698b14ad85303b
HimanshuBari21 <himanshubari789@gmail.com>
2023-09-24 23:13:03 +0530

    algo was ok just added comments

--- anagram.js
+++ anagram.js
@@ -1,7 +1,13 @@
+// check if we can reorganize the str2 to create str1 or vice versa
+
 const isAnagram = (str1, str2) => {
-    if (str1.length != str2.length) {
+    
+    // return false if length don't match
+    if (str1.length !== str2.length) {
         return false
     }
+
+    // we will first sort them and check if we have an same element on same position in both of them
     for (let i = 0; i < str1.length; i++) {
         if (str1.split("").sort()[i] == str2.split("").sort()[i]) {
             continue
@@ -13,5 +19,5 @@ const isAnagram = (str1, str2) => {
     return true
 }
 
-console.log(isAnagram("himmat", "haimatm")); //false
-console.log(isAnagram("himmat", "mihmat")); //true
+console.info(isAnagram("himmat", "haimat")); //false
+console.info(isAnagram("himmat", "mihmat")); //true
